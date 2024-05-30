15537f7b12a9ff95b14cd2787499be404b6bc18b
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-30 13:31:42 +0700

    Set up routing with useRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useRoutes} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+
+const ROUTE_PATHS = {
+    HOME: '/',
+    LOGIN: '/login',
+    REGISTER: '/register',
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const routes = useRoutes([
+        { path: ROUTE_PATHS.HOME, element: <HomePage /> },
+        { path: ROUTE_PATHS.LOGIN, element: <LoginPage /> },
+        { path: ROUTE_PATHS.REGISTER, element: <RegisterPage /> },
+    ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <>
+          <main>
+              {routes}
+          </main>
+      </>
+
+  );
 }
 
 export default App
