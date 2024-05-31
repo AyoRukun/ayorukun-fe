1c4d263dcfda1e57496ddd642e5d45ad18a85eb5
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-31 17:20:51 +0700

    Implement RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,31 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../assets/ayorukun.svg";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import { register } from '../states/auth/authSlice';
 
 function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const { status, error } = useSelector((state) => state.auth);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert('Password dan konfirmasi password tidak cocok!');
+            return;
+        }
+
+        dispatch(register({ email, password }))
+            .unwrap()
+            .then(() => {
+                navigate('/login');
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    };
     return (
         <section className="container">
             <div className="card">
                 <div className="flex-wrapper">
-                    <form className="form-group">
+                    <form className="form-group" onSubmit={handleSubmit}>
                         <div className="form-content">
                             <h1 className="title">Sign Up</h1>
-                            <label htmlFor="nameField" className="visually-hidden">Name</label>
-                            <input id="nameField" className="input-field" type="text" placeholder="Name"
-                                   aria-label="Name"/>
                             <label htmlFor="emailField" className="visually-hidden">Email</label>
                             <input id="emailField" className="input-field" type="email" placeholder="Email"
-                                   aria-label="Email"/>
+                                   aria-label="Email"
+                                   value={email}
+                                   onChange={(e) => setEmail(e.target.value)}
+                            />
                             <label htmlFor="passwordField" className="visually-hidden">Password</label>
                             <input id="passwordField" className="input-field" type="password" placeholder="Password"
-                                   aria-label="Password"/>
-                            <label htmlFor="repeatPasswordField" className="visually-hidden">Repeat Password</label>
+                                   aria-label="Password"
+                                   value={password}
+                                   onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <label htmlFor="confirmPasswordField" className="visually-hidden">Confirm Password</label>
                             <input
-                                id="repeatPasswordField"
+                                id="confirmPasswordField"
                                 className="input-field"
                                 type="password"
-                                placeholder="Repeat Password"
-                                aria-label="Repeat Password"
+                                placeholder="Confirm Password"
+                                aria-label="Confirm Password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
                             />
                             <p className="signin-text">Have account? <Link to={"/login"}>Sign in here</Link></p>
                             <button className="signup-button">Sign Up</button>
