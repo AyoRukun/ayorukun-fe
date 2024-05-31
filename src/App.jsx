e87e9d72ae7cfa5a638a09bcc847ed980afc3f0a
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-31 17:19:01 +0700

    Implement App component with routing and auth check

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import {useRoutes} from "react-router-dom";
+import React, {useEffect} from "react";
+import { useRoutes } from "react-router-dom";
+import {ROUTE_PATHS} from "./routes/index.js";
+import {checkAuth} from "./states/auth/authSlice.js";
+import {useDispatch, useSelector} from "react-redux";
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 
-const ROUTE_PATHS = {
-    HOME: '/',
-    LOGIN: '/login',
-    REGISTER: '/register',
-};
+const routeConfig  = [
+    { path: ROUTE_PATHS.HOME, element: <HomePage /> },
+    { path: ROUTE_PATHS.LOGIN, element: <LoginPage /> },
+    { path: ROUTE_PATHS.REGISTER, element: <RegisterPage /> },
+];
 
 function App() {
-    const routes = useRoutes([
-        { path: ROUTE_PATHS.HOME, element: <HomePage /> },
-        { path: ROUTE_PATHS.LOGIN, element: <LoginPage /> },
-        { path: ROUTE_PATHS.REGISTER, element: <RegisterPage /> },
-    ]);
+    const routes = useRoutes(routeConfig);
+    const dispatch = useDispatch();
+    const { user, token } = useSelector(state => state.auth);
 
-  return (
-      <>
-          <main>
-              {routes}
-          </main>
-      </>
+    useEffect(() => {
+        if (token) {
+            dispatch(checkAuth());
+        }
+    }, [dispatch, token]);
 
-  );
+
+    return (
+        <div>
+            <main>{routes}</main>
+        </div>
+    );
 }
 
-export default App
+export default App;
