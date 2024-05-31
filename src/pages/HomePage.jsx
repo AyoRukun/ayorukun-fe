b389f3ee6c4d6d4509de1892dbd345dc5fcbe8a6
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-31 17:19:57 +0700

    Implement HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,35 @@
-import {Link} from "react-router-dom";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../states/auth/authSlice';
 
 function HomePage() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { user, token } = useSelector(state => state.auth);
+
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/login');
+    };
+
     return (
         <>
             <h1>HomePage</h1>
-            <Link to={"/login"}><h2>Login</h2></Link>
-            <Link to={"/register"}><h2>Register</h2></Link>
+
+            {user ? (
+                <>
+                    <p>Selamat datang, {user.name}!</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </>
+            ) : (
+                <>
+                    <Link to={"/login"}><h2>Login</h2></Link>
+                    <Link to={"/register"}><h2>Register</h2></Link>
+                </>
+            )}
         </>
-    )
+    );
 }
 
 export default HomePage;
