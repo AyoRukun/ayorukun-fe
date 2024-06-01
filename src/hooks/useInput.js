3f5868d3675330935ef87432f7d273b3fdcce61b
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-01 12:28:49 +0700

    Add useStringInput

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,13 +1,8 @@
 import { useState } from 'react';
 
-function useInput(defaultValue = '') {
-    const [value, setValue] = useState(defaultValue);
+export default function useStringInput() {
+    const [value, setValue] = useState('');
+    const handleValueChange = (event) => setValue(event.target.value);
 
-    const onValueChangeHandler = (event) => {
-        setValue(event.target.value);
-    };
-
-    return [value, onValueChangeHandler];
+    return [value, handleValueChange];
 }
-
-export default useInput;
