f8e7f2857d645a27330f567fa7ab25b5c867da88
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-01 12:39:55 +0700

    Update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ const ROUTE_PATHS = {
     HOME: "/",
     LOGIN: "/login",
     REGISTER: "/register",
+    SIGN_IN: "/sign-in",
+    SIGN_UP: "/sign-up",
+    DISCUSSION: "/discussion",
+    DISCUSSION_DETAIL: "/discussion/:id",
 };
 
 export {ROUTE_PATHS};
