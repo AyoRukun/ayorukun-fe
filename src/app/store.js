823a4de3cff8065c6c482e12c94823739e78f1b8
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-01 12:37:58 +0700

    Add discussionSlice reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../states/auth/authSlice';
+import discussionReducer from "../states/discussion/discusssionSlice";
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        discussion: discussionReducer,
     },
 });
 
