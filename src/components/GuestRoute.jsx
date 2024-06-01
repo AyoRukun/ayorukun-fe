94408dbcfa207b9bd4ec03f56ca02a38133a25f6
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-01 12:32:56 +0700

    Implement GuestRoot component

---
+++ src/components/GuestRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {ROUTE_PATHS} from "../routes/index.js";
+
+const GuestRoute = ({ element }) => {
+    const { isAuthenticated } = useSelector(state => state.auth);
+
+    return isAuthenticated ? <Navigate to={ROUTE_PATHS.HOME} /> : element;
+};
+
+export default GuestRoute;
