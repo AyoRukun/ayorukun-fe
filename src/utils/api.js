fae7ec5da9a9cfdce3fe792e9d803f5fb34c8495
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-11 10:23:20 +0700

    Add report api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://backend.ayorukun.site';
+export const BASE_URL = 'https://backend.ayorukun.site';
 
 export async function registerUser(email, password) {
   try {
@@ -81,6 +81,83 @@ export async function createDiscussionComment(discussionId, commentData) {
 export async function getDiscussionDetail(discussionId) {
   try {
     const response = await axios.get(`${BASE_URL}/discussions/${discussionId}`);
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : new Error('Network error');
+  }
+}
+
+export async function searchRegion(query) {
+  try {
+    const token = localStorage.getItem('token');
+    const headers = { Authorization: `Bearer ${token}` };
+    const response = await axios.get(`${BASE_URL}/region/province-regency?q=${query}`, { headers });
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : new Error('Network error');
+  }
+}
+
+export async function createReport(reportData) {
+  try {
+    const token = localStorage.getItem('token');
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'multipart/form-data',
+    };
+
+    const formData = new FormData();
+
+    reportData.report_files.forEach((file) => {
+      formData.append('files', file);
+    });
+
+    formData.append('title', reportData.title);
+    formData.append('content', reportData.content);
+    formData.append('school_name', reportData.school_name);
+    formData.append('case_date', reportData.case_date);
+    formData.append('report_as', reportData.report_as);
+    formData.append('region', reportData.region);
+
+    const response = await axios.post(`${BASE_URL}/reports`, formData, { headers });
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : new Error('Network error');
+  }
+}
+
+export async function createReportComment(reportId, commentData) {
+  try {
+    const token = localStorage.getItem('token');
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    const response = await axios.post(
+        `${BASE_URL}/reports/${reportId}/comments`,
+        commentData,
+        { headers }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : new Error('Network error');
+  }
+}
+
+export async function getReportList() {
+  try {
+    const response = await axios.get(`${BASE_URL}/reports`);
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : new Error('Network error');
+  }
+}
+
+export async function getReportDetail(reportId) {
+  try {
+    const response = await axios.get(`${BASE_URL}/reports/${reportId}`)
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : new Error('Network error');
