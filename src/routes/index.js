185ff3af7522b778a41851e7b686bc5f156998f1
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-11 10:15:37 +0700

    Add report detail route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const ROUTE_PATHS = {
     HOME: "/",
-    LOGIN: "/login",
-    REGISTER: "/register",
     SIGN_IN: "/sign-in",
     SIGN_UP: "/sign-up",
     DISCUSSION: "/discussion",
     DISCUSSION_DETAIL: "/discussion/:id",
     REPORT: "/report",
+    REPORT_DETAIL: "/report/:id",
     ABOUT: "/about",
 };
 
