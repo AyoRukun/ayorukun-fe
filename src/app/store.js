ab448c4ee3cbda3a9083a3b1b4ed96d2e454beda
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-11 10:25:32 +0700

    Add report and region to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../states/auth/authSlice';
 import discussionReducer from "../states/discussion/discusssionSlice";
+import regionReducer from "../states/region/regionSlice.js";
+import reportReducer from "../states/report/reportSlice.js";
+import {loadingBarReducer} from "react-redux-loading-bar";
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         discussion: discussionReducer,
+        region: regionReducer,
+        report: reportReducer,
+        loadingBar: loadingBarReducer,
     },
 });
 
